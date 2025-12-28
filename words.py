c6781284366cbe8525da897389a70aa9296dc57a
Art3mis_XP <northwalker676@gmail.com>
2025-12-28 00:42:09 -0500

    Add files via upload

---
+++ words.py
@@ -0,0 +1,54 @@
+words = [
+    # A
+    "apple", "ant", "astronaut", "anchor",
+    # B
+    "banana", "ball", "butterfly", "bridge",
+    # C
+    "cat", "car", "castle", "cloud",
+    # D
+    "dog", "dragon", "desk", "diamond",
+    # E
+    "elephant", "eagle", "engine", "earth",
+    # F
+    "fish", "flower", "forest", "feather",
+    # G
+    "guitar", "garden", "galaxy", "ghost",
+    # H
+    "house", "horse", "honey", "helmet",
+    # I
+    "island", "ice", "insect", "idea",
+    # J
+    "jungle", "jacket", "jewel", "journey",
+    # K
+    "kangaroo", "key", "kite", "king",
+    # L
+    "lion", "lamp", "lake", "ladder",
+    # M
+    "mountain", "moon", "mirror", "machine",
+    # N
+    "notebook", "night", "needle", "nature",
+    # O
+    "ocean", "orange", "owl", "orbit",
+    # P
+    "pencil", "planet", "piano", "puzzle",
+    # Q
+    "quartz", "queen", "quest", "quiet",
+    # R
+    "rainbow", "river", "robot", "rocket",
+    # S
+    "sun", "star", "snake", "storm",
+    # T
+    "turtle", "tree", "train", "tower",
+    # U
+    "umbrella", "unicorn", "uniform", "universe",
+    # V
+    "violin", "valley", "vehicle", "volcano",
+    # W
+    "water", "whale", "window", "wind",
+    # X
+    "xylophone", "x-ray", "xenon", "xerox",
+    # Y
+    "yacht", "yellow", "yogurt", "yeti",
+    # Z
+    "zebra", "zoo", "zipper", "zone"
+]
